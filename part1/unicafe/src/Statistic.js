5ee4a4c0c2476b0a3259e440dd85e7616aaf3b8e
José Victor <jvictormmendesd@gmail.com>
2022-10-28 16:37:31 -0300

    adding more statistics on unicafe

--- part1/unicafe/src/Statistic.js
+++ part1/unicafe/src/Statistic.js
@@ -1,10 +1,17 @@
-const Statistic = ({good, neutral, bad}) => {
+const Statistic = ({ good, neutral, bad }) => {
+  const getAll = () => good + neutral + bad;
+  const getAverege = () => getAll() / 3;
+  const getPositive = () => (good / getAll()) * 100;
+
   return (
     <div>
       <h1>statistic</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {getAll()}</p>
+      <p>averege {getAverege()}</p>
+      <p>positive {getPositive()}%</p>
     </div>
   );
 };
