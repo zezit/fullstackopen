f75cd8204e9634db0ca2f559b368617af2939a1c
José Victor <jvictormmendesd@gmail.com>
2022-10-29 15:53:01 -0300

    phonebook step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,53 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Persons from "./Persons";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      id: 0,
-      name: "Arto Hellas",
-    },
+    { id: 1, name: "Arto Hellas", number: "040-1234567" },
+    { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
+    { id: 3, name: "Dan Abramov", number: "12-43-234345" },
+    { id: 4, name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
 
-  const [newName, setNewName] = useState("");
+  const [searchName, setSearchName] = useState("");
+  const [foundList, setFoundList] = useState([]);
 
-  const addNewName = (event) => {
+  useEffect(() => {
+    let foundListAux = [];
+
+    if (searchName) {
+      persons.forEach((person) => {
+        //* Verificação dentro de todo o nome
+        // if (
+        //   person.name
+        //     .toLocaleLowerCase()
+        //     .includes(searchName.toLocaleLowerCase())
+        // ) {
+        //   foundListAux.push(person);
+        // }
+
+        //* Verificação apenas no início do nome
+        if (
+          person.name
+            .toLocaleLowerCase()
+            .startsWith(searchName.toLocaleLowerCase())
+        ) {
+          foundListAux.push(person);
+        }
+      });
+
+      console.log(foundListAux);
+      setFoundList(foundListAux);
+    } else {
+      setFoundList([]);
+    }
+  }, [searchName]);
+
+  // Adiciona pessoa na lista
+  const addNewPerson = (event) => {
     event.preventDefault();
     let nameAux = event.target[0].value;
+    let numberAux = event.target[1].value;
     let personsAux = [...persons];
     let exists = false;
 
@@ -29,8 +63,13 @@ const App = () => {
       personsAux.push({
         id: persons.length,
         name: nameAux,
+        number: numberAux,
       });
-      setPersons(personsAux);
+      if (event.target[0].value && event.target[1].value) {
+        setPersons(personsAux);
+      } else {
+        alert(`Fill in all fields!`);
+      }
     } else {
       alert(`${nameAux} is already added to phonebook!`);
     }
@@ -39,20 +78,30 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form
-        onSubmit={addNewName}
-        onChange={({target}) => setNewName(target.value)}
-      >
+      <form onChange={({ target }) => setSearchName(target.value)}>
+        <div>
+          filter shown with <input />
+        </div>
+        <Persons persons={foundList} />
+      </form>
+
+      <h2>add a new</h2>
+      <form onSubmit={addNewPerson}>
         <div>
           name: <input />
         </div>
+
+        <div>
+          number: <input />
+        </div>
+
         <div>
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
       <Persons persons={persons} />
-      <div>debug: {newName}</div>
     </div>
   );
 };
--- part2/phonebook/src/Persons.js
+++ part2/phonebook/src/Persons.js
@@ -1,5 +1,10 @@
 const Persons = ({ persons }) => {
-  return persons.map((person) => <p key={person.id}>{person.name}</p>);
+  return persons.map((person) => (
+    <p key={person.id}>
+      <span>{person.name}</span>
+      <span style={{ margin: "0px 20px" }}>{person.number}</span>
+    </p>
+  ));
 };
 
 export default Persons;
