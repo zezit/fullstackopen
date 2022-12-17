78d4fb2b0796ef541b15e7a2d02d326c76822512
José Victor <victdiasmd@gmail.com>
2022-12-17 13:14:08 -0300

    request from openweathermap now working

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,24 +1,24 @@
+import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
-  const [countries, setCountries] = useState([])
-  const [foundCountries, setFoundCountries] = useState([])
-  const [nameSearch, setNameSearch] = useState("")
-  const [weather, setWeather] = useState({})
+  const [countries, setCountries] = useState([]);
+  const [foundCountries, setFoundCountries] = useState([]);
+  const [nameSearch, setNameSearch] = useState("");
+  const [temp, setTemp] = useState(0);
+  const [icon, setIcon] = useState("");
+  const [windSpeed, setWindSpeed] = useState(0);
 
   useEffect(() => {
-    axios.get("https://restcountries.com/v3.1/all")
-      .then((response) => {
-        setCountries(response.data)
-      })
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      setCountries(response.data);
+    });
   }, []);
-
 
   useEffect(() => {
     searchCountry();
   }, [nameSearch]);
-
 
   const searchCountry = () => {
     let foundCountries = [];
@@ -30,7 +30,6 @@ const App = () => {
           country.name.official
             .toLocaleLowerCase()
             .includes(nameSearch.toLocaleLowerCase()) ||
-
           country.name.common
             .toLocaleLowerCase()
             .includes(nameSearch.toLocaleLowerCase())
@@ -39,89 +38,98 @@ const App = () => {
         }
       });
 
-      console.log("found:", foundCountries);
+      // console.log("found:", foundCountries);
 
       setFoundCountries(foundCountries);
     } else {
       setFoundCountries([]);
     }
-  }
+  };
 
   const renderSpecific = (index) => {
-    let auxFond = []
-    auxFond.push(foundCountries[index])
+    let auxFond = [];
+    auxFond.push(foundCountries[index]);
 
-    setFoundCountries(auxFond)
-  }
+    setFoundCountries(auxFond);
+  };
 
   const renderName = () => {
-    if (foundCountries.length === 1) {
-      let showCountry = foundCountries[0]
-
-      axios.get(`${process.env.REACT_APP_API_LINK}&q=${showCountry.capital}&aqi=yes&key=${process.env.REACT_APP_API_KEY}`)
-      .then((response) => {
-        console.log("weather", response.data.current)
-        setWeather(response.data)
-      })
+    if (foundCountries.length == 1) {
+      // console.log(foundCountries[0].capital[0]);
+      axios
+        .get(
+          `${process.env.REACT_APP_API_WEATHER_LINK}?q=${foundCountries[0].capital}&units=metric&appid=${process.env.REACT_APP_API_WEATHER_KEY}`
+        )
+        .then((response) => {
+          // console.log("weather", response.data);
+          setTemp(response.data.main.temp);
+          setIcon(response.data.weather[0].icon);
+          setWindSpeed(response.data.wind.speed);
+        })
+        .catch((err) => {
+          console.log("ERRO:", err);
+        });
 
       const languages = () => {
-        let langs = []
+        let langs = [];
 
-        Object.keys(showCountry.languages).forEach((index) => {
-          langs.push(showCountry.languages[index])
-        })
+        Object.keys(foundCountries[0].languages).forEach((index) => {
+          langs.push(foundCountries[0].languages[index]);
+        });
 
         return (
           <ul>
-            {langs.map((arg, index) => <li key={index}>{arg}</li>)}
+            {langs.map((arg, index) => (
+              <li key={index}>{arg}</li>
+            ))}
           </ul>
-        )
-      }
-
-      console.log("W:", weather)
-      console.log("Temp:", weather.temp_c)
-      console.log("Icon:", weather.condition.icon)
-      console.log("Wind:", weather.wind_kph)
+        );
+      };
 
       return (
         <div>
-          <h1>{showCountry.name.common} - {showCountry.name.official}</h1>
-          <p>Capital: {showCountry.capital}</p>
-          <p>Area: {showCountry.area} km²</p>
+          <h1>
+            {foundCountries[0].name.common} - {foundCountries[0].name.official}
+          </h1>
+          <p>Capital: {foundCountries[0].capital}</p>
+          <p>Area: {foundCountries[0].area} km²</p>
 
           <h3>Languages:</h3>
 
           {languages()}
-          <div><img src={showCountry.flags.png} alt={`${showCountry.name.common}_flag`} /></div>
+          <div>
+            <img
+              src={foundCountries[0].flags.png}
+              alt={`${foundCountries[0].name.common}_flag`}
+            />
+          </div>
 
           <div>
-            <h1>Weather in {showCountry.name.common}</h1>
-            {/* <p>Temperature: {weatherAux[0].temp_c} °C</p> */}
-            {/* <img src={weatherAux[0].condition.icon} /> */}
-            {/* <p>Wind: {weatherAux[0].wind_kph} km/h</p> */}
+            <h1>Weather in {foundCountries[0].name.common}</h1>
+            <p>Temperature: {temp} °C</p>
+            <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} />
+            <p>Wind Speed: {windSpeed} km/h</p>
           </div>
         </div>
-      )
-    }
-
-    else if (foundCountries.length <= 10) {
-      return (foundCountries.map((nameCountry, i) =>
+      );
+    } else if (foundCountries.length <= 10) {
+      return foundCountries.map((nameCountry, i) => (
         <div key={i}>
           <p>
             <span>{nameCountry.name.official}</span>
             <button
               style={{ margin: "0 20px" }}
               onClick={() => renderSpecific(i)}
-            >show</button>
+            >
+              show
+            </button>
           </p>
         </div>
-      ))
+      ));
+    } else {
+      return <p>Too many matches, specify another filter</p>;
     }
-
-    else {
-      return (<p>Too many matches, specify another filter</p>)
-    }
-  }
+  };
 
   return (
     <div>
@@ -130,11 +138,9 @@ const App = () => {
         <input onChange={({ target }) => setNameSearch(target.value)} />
       </form>
 
-      <div>
-        {renderName()}
-      </div>
+      <div>{renderName()}</div>
     </div>
   );
-}
+};
 
 export default App;
